00be345ab04f946ebd2f3b0dc9a4131f52f43b49
Morris Stephens <46266209+moeblaze@users.noreply.github.com>
2025-12-11 13:08:25 -0500

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,2 @@
 // Minimal JS hook – extend later if you want interactive scans, metrics, etc.
-console.log("MoeCommunityCloud Hub v2 with subscriptions loaded.");
+console.log("MoeCommunityCloud Hub v3 with playbook images loaded.");
